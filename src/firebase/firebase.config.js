6e0914667696b3f4e45a5bf2f981019a23e4dacb
Md Ataullah <ataullahm100@gmail.com>
2024-04-26 00:41:26 +0600

    Fiebase Setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APPID,
 };
 
-// Initialize Firebase
+// Initialize Firebase Authentication
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth;
