b3f7815cd27142ef4fef4167973bdfd2deba8eb3
Md Ataullah <ataullahm100@gmail.com>
2024-04-26 11:35:31 +0600

    Banner section added

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+const Banner = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="mt-14 md:mt-20">
+        <h1 className="text-5xl text-center font-bold py-5">
+          This is Home Page
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
