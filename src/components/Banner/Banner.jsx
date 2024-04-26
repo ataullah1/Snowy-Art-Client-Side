ece1dd2b07753a0fa8ed5c7191693fcd19bf2cdb
Md Ataullah <ataullahm100@gmail.com>
2024-04-26 11:40:50 +0600

    Swipper slider added for banner section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,45 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+// import required modules
+import {
+  Autoplay,
+  Pagination,
+  Navigation,
+  Keyboard,
+  EffectFade,
+} from 'swiper/modules';
+
 const Banner = () => {
   return (
     <div className="min-h-screen">
       <div className="mt-14 md:mt-20">
-        <h1 className="text-5xl text-center font-bold py-5">
-          This is Home Page
-        </h1>
+        <Swiper
+          spaceBetween={30}
+          effect={'fade'}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[EffectFade, Navigation, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   );
