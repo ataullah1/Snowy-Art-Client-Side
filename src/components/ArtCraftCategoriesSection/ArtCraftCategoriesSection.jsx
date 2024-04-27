7ae41c5df9006023b2da13fbde548bfb770d7192
Md Ataullah <ataullahm100@gmail.com>
2024-04-27 15:07:43 +0600

    Categoris data showing UI

--- src/components/ArtCraftCategoriesSection/ArtCraftCategoriesSection.jsx
+++ src/components/ArtCraftCategoriesSection/ArtCraftCategoriesSection.jsx
@@ -1,15 +1,8 @@
-// import img1 from '../../assets/categories/img1.jpg';
-import img2 from '../../assets/categories/img2.jpg';
-// import img3 from '../../assets/categories/img3.jpg';
-// import img4 from '../../assets/categories/img4.jpg';
-// import img5 from '../../assets/categories/img6.jpg';
-// import img6 from '../../assets/categories/img7.jpg';
-
 import { PropTypes } from 'prop-types';
 
 const ArtCraftCategoriesSection = ({ data }) => {
   return (
-    <div>
+    <div className='pb-10'>
       <div>
         <h1 className="text-3xl md:text-5xl text-center pt-5 font-bold pb-2">
           Art & Craft Categories
@@ -19,14 +12,17 @@ const ArtCraftCategoriesSection = ({ data }) => {
         </p>
       </div>
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {data.map((dta, i) => (
-          <div key={i} className="border-2 border-firstColor rounded-lg">
+        {data.map((dta) => (
+          <div
+            key={dta._id}
+            className="border-2 border-firstColor rounded-lg hover:scale-[1.03] duration-200"
+          >
             <div className="cursor-pointer">
               <div className="rounded-t-md h-52 w-full overflow-hidden">
                 <div
                   className="h-full bg-cover bg-no-repeat p-12 text-center rounded-t-md relative hover:scale-110 hover:-rotate-3 duration-[2s]"
                   style={{
-                    backgroundImage: `url(${img2})`,
+                    backgroundImage: `url(${dta.pic})`,
                     backgroundPosition: 'center',
                     backgroundSize: 'cover',
                     backgroundRepeat: 'no-repeat',
@@ -44,144 +40,6 @@ const ArtCraftCategoriesSection = ({ data }) => {
             </div>
           </div>
         ))}
-        {/* <div className="border-2 border-firstColor rounded-lg">
-          <div className="cursor-pointer">
-            <div className="rounded-t-md h-52 w-full overflow-hidden">
-              <div
-                className="h-full bg-cover bg-no-repeat p-12 text-center rounded-t-md relative hover:scale-110 hover:-rotate-3 duration-[2s]"
-                style={{
-                  backgroundImage: `url(${img1})`,
-                  backgroundPosition: 'center',
-                  backgroundSize: 'cover',
-                  backgroundRepeat: 'no-repeat',
-                }}
-              >
-                <div
-                  className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed rounded-t-md flex items-center justify-center"
-                  style={{ backgroundColor: ' rgba(0, 0, 0, 0.4)' }}
-                ></div>
-              </div>
-            </div>
-            <h1 className="text-3xl text-center font-bold py-3 px-2">
-              Landscape Painting
-            </h1>
-          </div>
-        </div>
-        <div className="border-2 border-firstColor rounded-lg">
-          <div className="cursor-pointer">
-            <div className="rounded-t-md h-52 w-full overflow-hidden">
-              <div
-                className="h-full bg-cover bg-no-repeat p-12 text-center rounded-t-md relative hover:scale-110 hover:-rotate-3 duration-[2s]"
-                style={{
-                  backgroundImage: `url(${img2})`,
-                  backgroundPosition: 'center',
-                  backgroundSize: 'cover',
-                  backgroundRepeat: 'no-repeat',
-                }}
-              >
-                <div
-                  className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed rounded-t-md flex items-center justify-center"
-                  style={{ backgroundColor: ' rgba(0, 0, 0, 0.4)' }}
-                ></div>
-              </div>
-            </div>
-            <h1 className="text-3xl text-center font-bold py-3 px-2">
-              Portrait Drawing
-            </h1>
-          </div>
-        </div>
-        <div className="border-2 border-firstColor rounded-lg">
-          <div className="cursor-pointer">
-            <div className="rounded-t-md h-52 w-full overflow-hidden">
-              <div
-                className="h-full bg-cover bg-no-repeat p-12 text-center rounded-t-md relative hover:scale-110 hover:-rotate-3 duration-[2s]"
-                style={{
-                  backgroundImage: `url(${img3})`,
-                  backgroundPosition: 'center',
-                  backgroundSize: 'cover',
-                  backgroundRepeat: 'no-repeat',
-                }}
-              >
-                <div
-                  className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed rounded-t-md flex items-center justify-center"
-                  style={{ backgroundColor: ' rgba(0, 0, 0, 0.4)' }}
-                ></div>
-              </div>
-            </div>
-            <h1 className="text-3xl text-center font-bold py-3 px-2">
-              Watercolour Painting
-            </h1>
-          </div>
-        </div>
-        <div className="border-2 border-firstColor rounded-lg">
-          <div className="cursor-pointer">
-            <div className="rounded-t-md h-52 w-full overflow-hidden">
-              <div
-                className="h-full bg-cover bg-no-repeat p-12 text-center rounded-t-md relative hover:scale-110 hover:-rotate-3 duration-[2s]"
-                style={{
-                  backgroundImage: `url(${img4})`,
-                  backgroundPosition: 'center',
-                  backgroundSize: 'cover',
-                  backgroundRepeat: 'no-repeat',
-                }}
-              >
-                <div
-                  className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed rounded-t-md flex items-center justify-center"
-                  style={{ backgroundColor: ' rgba(0, 0, 0, 0.4)' }}
-                ></div>
-              </div>
-            </div>
-            <h1 className="text-3xl text-center font-bold py-3 px-2">
-              Oil Painting
-            </h1>
-          </div>
-        </div>
-        <div className="border-2 border-firstColor rounded-lg">
-          <div className="cursor-pointer">
-            <div className="rounded-t-md h-52 w-full overflow-hidden">
-              <div
-                className="h-full bg-cover bg-no-repeat p-12 text-center rounded-t-md relative hover:scale-110 hover:-rotate-3 duration-[2s]"
-                style={{
-                  backgroundImage: `url(${img5})`,
-                  backgroundPosition: 'center',
-                  backgroundSize: 'cover',
-                  backgroundRepeat: 'no-repeat',
-                }}
-              >
-                <div
-                  className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed rounded-t-md flex items-center justify-center"
-                  style={{ backgroundColor: ' rgba(0, 0, 0, 0.4)' }}
-                ></div>
-              </div>
-            </div>
-            <h1 className="text-3xl text-center font-bold py-3 px-2">
-              Charcoal Sketching
-            </h1>
-          </div>
-        </div>
-        <div className="border-2 border-firstColor rounded-lg">
-          <div className="cursor-pointer">
-            <div className="rounded-t-md h-52 w-full overflow-hidden">
-              <div
-                className="h-full bg-cover bg-no-repeat p-12 text-center rounded-t-md relative hover:scale-110 hover:-rotate-3 duration-[2s]"
-                style={{
-                  backgroundImage: `url(${img6})`,
-                  backgroundPosition: 'center',
-                  backgroundSize: 'cover',
-                  backgroundRepeat: 'no-repeat',
-                }}
-              >
-                <div
-                  className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed rounded-t-md flex items-center justify-center"
-                  style={{ backgroundColor: ' rgba(0, 0, 0, 0.4)' }}
-                ></div>
-              </div>
-            </div>
-            <h1 className="text-3xl text-center font-bold py-3 px-2">
-              Cartoon Drawing
-            </h1>
-          </div>
-        </div> */}
       </div>
     </div>
   );
