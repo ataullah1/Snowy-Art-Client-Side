1860ad3c64c4de0124219aee394981365092058f
Md Ataullah <ataullahm100@gmail.com>
2024-04-29 13:07:11 +0600

    Simple design change in details page

--- src/pages/ViewDetailsPage/ArtAndCraftDetails.jsx
+++ src/pages/ViewDetailsPage/ArtAndCraftDetails.jsx
@@ -1,4 +1,4 @@
-import { Link, useLoaderData, useParams } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
 import img1 from '../../assets/banner/img4.png';
 import { FaStar } from 'react-icons/fa';
 import { FaRegStarHalfStroke } from 'react-icons/fa6';
@@ -46,29 +46,26 @@ const ArtAndCraftDetails = () => {
   return (
     <div>
       {/* Top Banner Img */}
-      <div className="h-48 md:h-64 bg-fuchsia-100">
-        <div className="h-full w-full overflow-hidden">
+      <div className="h-52 md:h-72 bg-fuchsia-100">
+        <div className=" h-full w-full overflow-hidden">
           <div
-            className="h-full bg-cover bg-no-repeat p-12 text-center relative hover:scale-110 hover:-rotate-1 duration-[2s] "
+            className="h-full bg-cover bg-no-repeat p-12 text-center  relative hover:scale-110 hover:-rotate-1 duration-[2s] "
             style={{
               backgroundImage: `url(${img1})`,
-              backgroundPosition: '0 -100px',
+              backgroundPosition: 'top',
               backgroundSize: 'cover',
               backgroundRepeat: 'no-repeat',
             }}
           >
             <div
-              className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed flex items-center justify-center"
+              className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed  flex items-center justify-center"
               style={{ backgroundColor: ' rgba(0, 0, 0, 0.6)' }}
             ></div>
           </div>
-          <div className="absolute z-30 left-1/2 -translate-x-1/2 text-2xl sm:text-3xl text-white font-bold text-center top-28 md:top-36 w-full italic">
-            <Link to={'/'}>
-              <button className="italic">Home /</button>
-            </Link>{' '}
-            <Link to={'/all-art-craft-items'}>
-              <button className="italic"> All Items</button>
-            </Link>
+          <div className="w-72 sm:w-[350px] md:w-[550px] absolute z-30 left-1/2 -translate-x-1/2 text-2xl sm:text-3xl text-white md:text-5xl font-bold top-32 md:top-44">
+            <h1 className="border-b-4 border-secondColor text-center  px-2">
+              {itemName}
+            </h1>
           </div>
         </div>
       </div>
